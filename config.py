781bd91922cf78eb7cd676ee2b557e7ed11a640f
Jonas Frugte <jonasfrugte@gmail.com>
2026-07-31 12:11:46 +0200

    config err

--- config.py
+++ config.py
@@ -12,7 +12,6 @@ LIGHTCONE_L2p8_m9 = FLAMINGO_L2p8_m9 / "lightcones"
 MASS_MAP_L2p8_m9_4096 = FLAMINGO_L2p8_m9 / "mass_maps_4096"
 SOAP_L2p8_m9 = FLAMINGO_L2p8_m9 / "soap"
 SHELLS_RESOLVED_L2p8_m9 = FLAMINGO_L2p8_m9 / "shells_resolved"
-SHELLS_RESOLVED_L2p8_m9_DUPLICATION_CHECK = HOME / 
 
 # minimum number of particles for a shape tensor to be "well determined"
 MIN_NUM_PARTICLES_FOR_SHAPE = 300
