5920e9726c65b3db9cea2e3084125de0bdd9ca36
Jonas Frugte <jonasfrugte@gmail.com>
2026-08-09 19:38:46 +0200

    sh stuff

--- job_scripts/lensing_pert_alt.sh
+++ job_scripts/lensing_pert_alt.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-#PBS -N lens_pert_alt
-#PBS -o /nethome/frugt001/galaxy_b_modes/job_scripts/lens_pert_alt.out
-#PBS -e /nethome/frugt001/galaxy_b_modes/job_scripts/lens_pert_alt.err
-#PBS -l walltime=12:00:00
-#PBS -l nodes=1:ppn=1
-#PBS -l mem=64gb
-#PBS -m aeb
-#PBS -M j.s.a.frugte@uu.nl
+#$ -N lens_pert_alt
+#$ -o /nethome/frugt001/galaxy_b_modes/job_scripts/lens_pert_alt.out
+#$ -e /nethome/frugt001/galaxy_b_modes/job_scripts/lens_pert_alt.err
+#$ -q itf-fat.q
+#$ -l h_rt=12:00:00
+#$ -l h_vmem=64G
+#$ -pe smp 1
+#$ -cwd
+#$ -V
+#$ -m abe
+#$ -M j.s.a.frugte@uu.nl
 
-cd /nethome/frugt001/galaxy_b_modes
-export OMP_NUM_THREADS=$PBS_NUM_PPN
+export OMP_NUM_THREADS=$NSLOTS
 
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate b-modes
 python3 -u lensing_pert_alt.py
