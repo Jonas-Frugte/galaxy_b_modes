405d214420709d92207fab00e520e8ab0c3f7470
Jonas Frugte <jonasfrugte@gmail.com>
2026-09-04 23:32:54 +0200

    changed download .sh script

--- cluster/jobs/download_and_process_catalogues.sh
+++ cluster/jobs/download_and_process_catalogues.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+#$ -N download_and_process
+#$ -o /nethome/frugt001/galaxy_b_modes/cluster/jobs/logs/download_and_process.out
+#$ -e /nethome/frugt001/galaxy_b_modes/cluster/jobs/logs/download_and_process.err
+#$ -q itf-fat.q
+#$ -l h_rt=72:00:00
+#$ -l h_vmem=64G
+#$ -pe smp 1
+#$ -cwd
+#$ -V
+#$ -m abe
+#$ -M j.s.a.frugte@uu.nl
+
 set -e
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate b-modes
 
-cd "$(dirname "$0")/../scripts"
+cd /nethome/frugt001/galaxy_b_modes/cluster/scripts
 
 python3 -u data_download_lightcones.py
 python3 -u data_download_mass_maps.py
