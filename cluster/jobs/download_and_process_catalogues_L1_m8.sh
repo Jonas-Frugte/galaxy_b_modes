336c64b51e704cfeea5a8c8b3f1fb6b6735c82e1
Jonas Frugte <jonasfrugte@gmail.com>
2026-09-14 10:10:38 +0200

    misc

--- cluster/jobs/download_and_process_catalogues_L1_m8.sh
+++ cluster/jobs/download_and_process_catalogues_L1_m8.sh
@@ -4,7 +4,7 @@
 #$ -e /nethome/frugt001/galaxy_b_modes/cluster/jobs/logs/download_and_process_L1_m8.err
 #$ -q itf-fat.q
 #$ -l h_rt=72:00:00
-#$ -l h_vmem=128G
+#$ -l h_vmem=200G
 #$ -pe smp 32
 #$ -cwd
 #$ -V
